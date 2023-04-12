4e9cf18751ac76036472417d98719fa9654b9ec6
angeldavid <angelcontreras0777@gmail.com>
2023-04-12 13:38:54 -0400

    Se modifico el boton de descarga y estilos

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,17 @@
-function imprimir(){
-    window.print();
-}
+(() => {
+  const button = document.querySelector("#pdf");
+  const resumeHtml = document.querySelector("body");
+
+  function generadorPDF() {
+    html2pdf().from(resumeHtml).save();
+  }
+
+  //Cuando el boton sea precionado descargar documento
+  button.addEventListener("click", async () => {
+    //quitar pdf
+    button.style.display = "none";
+    await generadorPDF();
+    //regresar el icono PDF
+    button.style.display = "block";
+  });
+})();
